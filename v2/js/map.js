08e1a2e5450a19e36e49eeab74b0be9ba7b06809
quantium <alexcollsoutumuro@gmail.com>
2023-01-21 11:59:48 +0100

    add beenzer in map

--- v2/js/map.js
+++ v2/js/map.js
@@ -1,7 +1,23 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 8,
+    zoom: 14,
     center: { lat: 35.717, lng: 139.731 },
+  });
+
+  const customIcon = "images/logo-1.png";
+  const marker = new google.maps.Marker({
+    position: { lat: 35.717, lng: 139.731 },
+    map: map,
+    icon: customIcon,
+  });
+
+  const image = "images/gifs/13.gif";
+  const infoWindow = new google.maps.InfoWindow({
+    content: `<img src="${image}" style="width:200px;height:200px;">`,
+  });
+
+  marker.addListener("click", function () {
+    infoWindow.open(map, marker);
   });
 }
 
